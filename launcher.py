b1c300fa9f130d1e9771a8f0f993d25709ba3b9b
Andriy Mykhaylyk <erp.lsf@gmail.com>
2020-04-29 22:11:51 +0200

    rewrite to use simpler pitch function and krpc streams

--- launcher.py
+++ launcher.py
@@ -1,5 +1,6 @@
 import krpc
 import time
+import math
 
 conn = krpc.connect(name='Launcher')
 vessel = conn.space_center.active_vessel
@@ -7,16 +8,12 @@ flight = vessel.flight()
 kerbin_flight = vessel.flight(vessel.orbit.body.reference_frame)
 
 
-def pitch_func(altitude):
-    # magic constants calculated from Desmos
-    a = -0.258652
-    b = 0.530676
-    k = -92.9603
-    pitch_angle = a * (altitude ** b) - k
+def pf(altitude):
+    pitch_angle = 90 * (1 - (altitude / 70_000) ** 0.5)
 
     if altitude < 0:
         return float(90)
-    elif altitude > 80_000:
+    elif pitch_angle < 0:
         return float(0)
     else:
         return pitch_angle
@@ -26,43 +23,52 @@ vessel.control.sas = False
 vessel.control.rcs = False
 vessel.control.throttle = 1.0
 
-vessel.control.activate_next_stage()
 vessel.auto_pilot.target_heading = 90
 vessel.auto_pilot.target_pitch = 90
+vessel.control.activate_next_stage()
 vessel.auto_pilot.engage()
 
 target_apoapsis = 90_000
+atmosphere_edge = 70_000
+a = pitch_factor(atmosphere_edge)
 target_apo_reached = False
 
 s3_resources_amount = conn.add_stream(vessel.resources_in_decouple_stage(stage=3, cumulative=False).amount, 'LiquidFuel')
 s3_separated = False
 
+altitude = conn.add_stream(getattr, flight, 'mean_altitude')
+apoapsis = conn.add_stream(getattr, vessel.orbit, 'apoapsis_altitude')
+pitch = conn.add_stream(getattr, flight, 'pitch')
+current_stage = conn.add_stream(getattr, vessel.control, 'current_stage')
+
+
+def current_decouple_stage():
+    return current_stage() - 1
+
+resources_in_current_stage = conn.add_stream(vessel.resources_in_decouple_stage(stage=current_decouple_stage(), cumulative=False).amount, 'LiquidFuel')
+
+
+def error(value, target_value):
+    return 1 - (value / target_value)
+
+
 while True:
-    # "gravity" turn
-    target_pitch = pitch_func(flight.mean_altitude)
-    current_pitch = flight.pitch
-    if (1 - (target_pitch / current_pitch)) > 0.01:
+    # print(f"alt: {altitude()}")
+    target_pitch = pf(altitude())
+    # print(f"tp: {target_pitch}")
+    # print(f"cp: {pitch()}")
+    if abs(error(pitch(), target_pitch)) > 0.01:
         vessel.auto_pilot.target_pitch = target_pitch
-    # speed = kerbin_flight.speed
-    # print(f"pitch diff: {current_pitch - target_pitch}")
 
-    if not s3_separated and s3_resources_amount() < 0.1:
+    # print(f"cs: {current_stage()}")
+    # print(f"r: {resources_in_current_stage()}")
+    if resources_in_current_stage() < 0.1:
         vessel.control.activate_next_stage()
-        s3_separated = True
+        time.sleep(1)
 
-    # apoapsis/throttle control
-    apoapsis = vessel.orbit.apoapsis_altitude
-    # print(f"current_apo: {apoapsis}, target: {target_apoapsis}")
-    if not target_apo_reached:
-        if (1 - (apoapsis / target_apoapsis)) < 0.05:
-            current_throttle = vessel.control.throttle
-            vessel.control.throttle = max(current_throttle * 0.9, 0.1)
-        elif (1 - (apoapsis / target_apoapsis)) < 0.01:
-            vessel.control.throttle = 0
-            target_apo_reached = True
-    if (1 - (apoapsis / target_apoapsis)) < 0.01:
+    # print(f"apo: {apoapsis()}")
+    if error(apoapsis(), target_apoapsis) < 0.1:
         print("Program finished, target apoapsis reached.")
         vessel.control.throttle = 0
         vessel.auto_pilot.disengage()
         break
-    time.sleep(1)
